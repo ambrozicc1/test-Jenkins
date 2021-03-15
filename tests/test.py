a97a87d9d4e8e6a20f660224bc76239034405789
ambrozicc1 <44813043+ambrozicc1@users.noreply.github.com>
2021-03-15 12:07:36 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -6,10 +6,6 @@ class TestStringMethods(unittest.TestCase):
   def test_isupper(self): #testing for isupper
     self.assertTrue('HELLO'.isupper())
     self.assertFalse('HELlO'.isupper())
-  def test_split(self): #testing for split
-    self.assertEqual('Hello World'.split(),['Hello','World'])
-    with self.assertRaises(TypeError):
-    'Hello World'.split(2)
     
 if __name__=='__main__':
         unittest.main()
