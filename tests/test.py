d45395efe80249338de9cf50d8c3c5c822a08c32
ambrozicc1 <44813043+ambrozicc1@users.noreply.github.com>
2021-03-19 11:15:09 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import unittest
+import xmlrunner
 
 class TestStringMethods(unittest.TestCase):
   def test_lstrip(self): #testing for left stripping
@@ -8,4 +9,8 @@ class TestStringMethods(unittest.TestCase):
     self.assertFalse('HELlO'.isupper())
     
 if __name__=='__main__':
-        unittest.main()
+  unittest.main(
+    testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+    # these make sure that some options that are not applicable
+    # remain hidden from the help menu.
+    failfast=False, buffer=False, catchbreak=False)
