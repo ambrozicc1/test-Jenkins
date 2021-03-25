a0ae7641a447c8c00a01bad671f531042d81bb12
ambrozicc1 <44813043+ambrozicc1@users.noreply.github.com>
2021-03-25 11:41:18 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -7,10 +7,12 @@ class TestStringMethods(unittest.TestCase):
   def test_isupper(self): #testing for isupper
     self.assertTrue('HELLO'.isupper())
     self.assertFalse('HELlO'.isupper())
+  def test_isTrue(self):
+    self.assertTrue(1*1*1)
+  def test_isFalse(self):
+    self.assertTrue(0)
     
 if __name__=='__main__':
   unittest.main(
     testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
-    # these make sure that some options that are not applicable
-    # remain hidden from the help menu.
     failfast=False, buffer=False, catchbreak=False)
